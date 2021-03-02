c3e7e917752085bb045f448c233697305d17f9b8
Oleksandr <olexander.dovgal@gmail.com>
2021-03-02 19:18:16 +0200

    added burger menu

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let mobileMenu = document.querySelector(".nav-mobile-menu");
-let maimMenu = document.querySelector(".navigation");
+let mainMenu = document.querySelector(".navigation");
 console.log(mobileMenu);
-console.log(maimMenu);
+console.log(mainMenu);
 
-mobileMenu.addEventListener("click", function() {
-	mobileMenu.classList.toggle('active-menu')
+mobileMenu.addEventListener("click",function() {
+	mobileMenu.classList.toggle("active-menu");
+	if(mobileMenu.classList.contains("active-menu")) {
+		mainMenu.classList.add("active-menu")
+	} else {
+		mainMenu.classList.remove("active-menu");
+	}
 })
